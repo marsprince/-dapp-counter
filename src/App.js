9269b706fda5981ca6eb5119d9d9de589c9e7c69
liujia02 <bit_liujia@163.com>
2023-03-08 09:35:11 +0800

    metamask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+
 function App() {
+
+  const [provider, setProvider] = useState(null)
+  const [defaultAccount, setDefaultAccount] = useState(null);
+
+  useEffect(() => {
+    if (typeof window.ethereum !== 'undefined') {
+      console.log('MetaMask is installed!');
+      console.log(window.ethereum);
+      setProvider(new ethers.BrowserProvider(window.ethereum))
+    }
+  }, [])
+
+  // connect wallet
+  const connectwalletHandler = async () => {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      setDefaultAccount(accounts[0])
+    }
+  }
+
+  // read counter
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        style={{ background: defaultAccount ? "#A5CC82" : "white" }}
+        onClick={connectwalletHandler}>
+        {defaultAccount ? "Connected!!" : "Connect"}
+      </button>
+      <div>
+        Address: {defaultAccount}
+      </div>
+
+      <div>
+
+      </div>
     </div>
   );
 }
