30254e555c5e08e3cff9f334f62165fc8f189f92
liujia02 <bit_liujia@163.com>
2023-03-10 14:19:56 +0800

    inc and dec

--- src/App.js
+++ src/App.js
@@ -10,25 +10,49 @@ import {
 
 function App() {
 
-  const [provider, setProvider] = useState(null)
   const [defaultAccount, setDefaultAccount] = useState(null);
   const [count, setCount] = useState(null)
+  const [contractCounter, setContractCounter] = useState(null)
+
+  const getProvider = () => {
+    return new ethers.BrowserProvider(window.ethereum)
+  }
 
   // read count from contract
-  const getCount = async (contractCounter) => {
+  const getCount = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum)
+    const contractCounter = new ethers.Contract(addressCounter, abiCounter, provider)
     const c = await contractCounter.get()
     setCount(toNumber(c))
+  }
+
+  // add
+  const addCount = async () => {
+    const tx = await contractCounter.inc()
+    await tx.wait()
+    getCount()
+  }
+
+  const decCount = async () => {
+    const tx = await contractCounter.dec()
+    await tx.wait()
+    getCount()
   }
 
   useEffect(() => {
     if (typeof window.ethereum !== 'undefined') {
       console.log('MetaMask is installed!');
       console.log(window.ethereum);
-      const p = new ethers.BrowserProvider(window.ethereum)
-      setProvider(p)
 
-      const contractCounter = new ethers.Contract(addressCounter, abiCounter, p)
-      getCount(contractCounter)
+      getCount()
+
+      const contractCounterSigner = async () => {
+        const signer = await getProvider().getSigner();
+        const contract = new ethers.Contract(addressCounter, abiCounter, signer)
+        setContractCounter(contract)
+      }
+
+       contractCounterSigner()
     }
   }, [])
 
@@ -54,7 +78,7 @@ function App() {
       </div>
 
       <div>
-        Count: {count}
+        <span style={{marginRight: '50px'}} onClick={decCount}>-</span> Count: {count} <span style={{marginLeft: '50px'}} onClick={addCount}>+</span>
       </div>
     </div>
   );
